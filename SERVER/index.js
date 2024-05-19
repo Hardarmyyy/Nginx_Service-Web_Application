974328c803d5835314d5a11993a51ca4bcd3072c
hardarmyyy <adeyemoadams5@gmail.com>
2024-05-19 02:19:26 +0100

    REFACTOR: - update folder file structure - add new mongodb database string

---
+++ SERVER/index.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+
+const userRoutes = require('./routes/userRoutes')
+
+
+//connect to database
+mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true})  
+.then(() => {
+
+    // Middlewares
+    app.use(express.json())
+
+    app.use(cors())
+
+    app.use((req, res, next) => {
+        console.log(req.path, req.method)  
+        next()
+    })
+
+
+    app.use('/api', userRoutes)
+
+    //listen for requests
+    const server = app.listen(process.env.PORT || PORT, ()=> {
+        console.log('Connected to Database and Server is listening and running on port ' + process.env.PORT)
+        process.send('ready', () => {
+            console.log('Server is ready to receive requests')
+        })
+    })
+    
+    // Handling graceful shutdown 
+    process.on('message', (msg) => {
+        if (msg == 'shutdown') {
+            console.log('Shutdown signal is received')
+            console.log('Closing all connections...and getting ready to close server')
+    
+            setTimeout(() => {
+                console.log('Shutting down server and all database connection...')
+                server.close(() => {
+                    mongoose.connection.close(false, () => {
+                        console.log('MongoDB connection closed')
+                    })
+                    console.log('server closed successfully')
+                    process.exit(0)
+                })
+            }, 1500)
+        }
+    })
+})
+.catch((err) => {
+        console.log(err)  
+}) 
+
+
+module.exports = app;
