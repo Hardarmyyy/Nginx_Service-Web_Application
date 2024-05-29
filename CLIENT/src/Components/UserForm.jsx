0ae9c17f8e73f3bafb227175e5f9042069f17534
hardarmyyy <adeyemoadams5@gmail.com>
2024-05-29 10:01:42 +0100

    FEAT: add UserForm component

---
+++ CLIENT/src/Components/UserForm.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import Button from './Button'
+
+
+const UserForm = ({user, error, handleFormSubmit, handleChange}) => {
+
+return (
+
+<>
+    <form className='w-3/4 text-my-primary font-Montserrat' onSubmit={handleFormSubmit}>
+
+        <div className='w-full flex items-center justify-between'>
+
+            <div className='w-1/2 mb-4 relative mr-2'>
+                <label className='font-bold'> First name <span className='text-crimson'> * </span></label> <br />
+                <input 
+                    type='text' 
+                    className='w-full px-2 py-1 border-2 rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-slate-600'
+                    value={user?.fName} 
+                    onChange={handleChange} 
+                    placeholder='Enter first name' 
+                    name='fname' 
+                    maxLength={30}
+                />
+                {error && <p className='text-crimson text-sm absolute left-0'> {error.fName} </p>}
+            </div>
+
+            <div className='w-1/2 mb-4 relative ml-2'>
+                <label className='font-bold'> Last name <span className='text-crimson'> * </span></label> <br />
+                <input 
+                    type='text' 
+                    className='w-full px-2 py-1 border-2 rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-slate-600'
+                    value={user?.lName} 
+                    onChange={handleChange} 
+                    placeholder='Enter last name' 
+                    name='lname' 
+                    maxLength={30}
+                />
+                {error && <p className='text-crimson text-sm absolute left-0'> {error.lName} </p>}
+            </div>
+
+        </div>
+        
+        <div className='mb-4 relative'>
+            <label className='font-bold'> Email <span className='text-crimson'> * </span></label> <br />
+            <input 
+                type='text'
+                className='w-full px-2 py-1 border-2 rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-slate-600'
+                value={user?.email} 
+                onChange={handleChange} 
+                placeholder='user@example.com' 
+                name='email' 
+            />
+            {error && <p className='text-crimson text-sm absolute left-0'> {error.email} </p>}
+        </div>
+
+        <div className='mb-4 relative'>
+            <label className='font-bold'> Role <span className='text-crimson'> * </span></label> <br />
+            <input 
+                type='text'
+                className='w-full px-2 py-1 border-2 rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-slate-600'
+                value={user?.role} 
+                onChange={handleChange} 
+                placeholder='your role' 
+                name='role' 
+            />
+            {error && <p className='text-crimson text-sm absolute left-0'> {error.role} </p>}
+        </div>
+
+        <div className='mb-4 relative'>
+            <label className='font-bold'> Github Link <span className='text-crimson'> * </span></label> <br />
+            <input 
+                type='text'
+                className='w-full px-2 py-1 border-2 rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-slate-600'
+                value={user?.github} 
+                onChange={handleChange} 
+                placeholder='Ex: https://github.com/username' 
+                name='github' 
+            />
+            {error && <p className='text-crimson text-sm absolute left-0'> {error.github} </p>}
+        </div>
+
+        <div className='text-center'>
+            <Button margin='20px 0px' padding='10px 70px'> {status === 'Loading.......' ? <span> Creating profile .... </span> : <span> Add profile </span>} </Button> 
+        </div>
+
+    </form>
+</>
+
+)
+}
+
+export default UserForm
--- CLIENT/src/Pages/Users/AddUser.jsx
+++ CLIENT/src/Pages/Users/AddUser.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import UserForm from '../../Components/UserForm'
 
 const AddUser = () => {
 
 return (
     <>
-        <h1> Add user page </h1>
+        <section className='flex flex-col items-center py-4'>
+
+            <h2 className='text-2xl font-bold my-4'> Add new profile </h2>
+
+            <UserForm></UserForm>
+
+        </section>
+        
     </>
 )
 }
