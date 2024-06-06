f3d8501b0d7eec1c06e21e00de3340a3f34b14a8
hardarmyyy <adeyemoadams005@gmail.com>
2024-06-06 11:38:43 +0100

    FIX: add sticky position to the navigation bar

--- CLIENT/src/Layouts/Navigation.jsx
+++ CLIENT/src/Layouts/Navigation.jsx
@@ -28,7 +28,7 @@ useEffect(() => {
 return (
 
 <>
-    <header className='w-full bg-black px-4 py-2 flex justify-between items-center'>
+    <header className='w-full bg-black sticky top-0 z-50'>
 
         <div className='w-full px-4 py-2 flex justify-between items-center'>
 
