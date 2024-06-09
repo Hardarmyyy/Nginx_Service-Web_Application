2329f1b8c29a17347be5e5fa9baba33c037d7c7c
hardarmyyy <adeyemoadams005@gmail.com>
2024-06-09 21:34:16 +0100

    STYLE: - add flowbite tailwind css components - add hamburger menu for mobile

--- CLIENT/tailwind.config.js
+++ CLIENT/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js"
   ],
   theme: {
     extend: {
@@ -66,5 +67,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
